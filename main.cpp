a65bd665fddba5d2603270eedd4cbbf4e271a6bb
RedWlf1999 <145369353+RedWlf1999@users.noreply.github.com>
2023-09-23 20:38:56 +0200

    Update main.cpp
    hibák javítva

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // hiányzó karakter az elemszámnál
-    std::cout << '1-100 ertekek duplazasa' // nincs lezárva a kiiratás
-    for (int i = 0;) // hiányos ciklusmag
+    int *b = new int[N_ELEMENTS]; // hiányzó karakter az elemszámnál
+    std::cout << '1-100 ertekek duplazasa'<<std::endl; // nincs lezárva a kiiratás
+    for (int i = 0;i<N_ELEMENTS;i++) // hiányos ciklusmag
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:" // nincs lezárva a kiiratás
+        std::cout << "Ertek:" <<std::endl;;// nincs lezárva a kiiratás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //a második vessző nem pontos
+    for (int i = 0; i < N_ELEMENTS; i++) //a második vessző nem pontos
     {
-        atlag += b[i] //hiányzó pontosvessző
+        atlag += b[i]; //hiányzó pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
